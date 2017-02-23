7153eb30d4903e43b5e3606807438d537bcc22fa
yzeaho <y-zeaho@qq.com>
2017-02-23 14:25:30 +0800

    修改客户反馈问题

---
+++ app/src/main/java/com/canice/wristbandapp/ble/BackgroundHandler.java
@@ -0,0 +1,57 @@
+package com.canice.wristbandapp.ble;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.Process;
+
+/**
+ * 后台线程
+ *
+ * @author y
+ */
+public class BackgroundHandler {
+
+    private static final Handler sHandler;
+
+    static {
+        HandlerThread sLooperThread = new HandlerThread("BackgroundHandler", Process.THREAD_PRIORITY_BACKGROUND);
+        sLooperThread.start();
+        sHandler = new Handler(sLooperThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                BackgroundTask task = (BackgroundTask) msg.obj;
+                if (task != null) {
+                    task.run();
+                }
+            }
+        };
+    }
+
+    private BackgroundHandler() {
+    }
+
+    public static void send(BackgroundTask task) {
+        if (task != null) {
+            int key = task.generateKey();
+            sHandler.removeMessages(key);
+            sHandler.sendMessage(sHandler.obtainMessage(key, task));
+        }
+    }
+
+    public static void post(Runnable runnable) {
+        sHandler.post(runnable);
+    }
+
+    public static void remove(Runnable runnable) {
+        sHandler.removeCallbacks(runnable);
+    }
+
+    public interface BackgroundTask extends Runnable {
+
+        /**
+         * 生产唯一标识key
+         */
+        int generateKey();
+    }
+}
