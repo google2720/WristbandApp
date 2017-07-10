5be7d917a0a43d22f45b78dc12c65b247f557919
yzeaho <y-zeaho@qq.com>
2017-07-10 19:49:32 +0800

    修改来电提醒的消息报文

--- app/src/main/java/com/canice/wristbandapp/ble/data/Remind.java
+++ app/src/main/java/com/canice/wristbandapp/ble/data/Remind.java
@@ -7,7 +7,7 @@ public class Remind extends Data {
         value[0] = (byte) 0xAA;
         value[1] = (byte) 0xE1;
         value[2] = Byte.parseByte("00001000", 2);
-//        value[3] = 0x01;
+        value[3] = Byte.parseByte("00001000", 2);
         value[4] = 0x01;
         System.arraycopy(phone, 0, value, 5, Math.min(phone.length, 14));
         value[19] = sum(value);
